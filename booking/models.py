1358db27647e860ee2c44aedb4a73f06765af3dc
neem99 <72668432+neem99@users.noreply.github.com>
2026-04-04 15:30:11 -0400

    Lead data model modifications, regenerate django migrations

--- booking/models.py
+++ booking/models.py
@@ -1,13 +1,13 @@
 from django.db import models
+from django.utils.timezone import now
+
 
 # Create your models here.
 # Lead table
-class Lead (models.Model):
-    full_name: models.CharField(max_length=100)
-    email: models.EmailField
-    created_at: models.DateTimeField(auto_now_add=True)
+class Lead(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(default="")
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.full_name} ({self.email})"
-
-
